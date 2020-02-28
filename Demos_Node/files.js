4c2bec02163faa8154d568fba52a3ff2e5620c5a
Danilo Novakovic <dakenzi97@gmail.com>
2020-02-28 15:27:28 +0100

    Refactored `files.js` and added example for creating/removing folders

--- Demos_Node/files.js
+++ Demos_Node/files.js
@@ -1,16 +1,20 @@
 const fs = require("fs");
 
+const readFilePath = "text_file.txt";
+const writeFilePath = "_text_file_copy.txt";
+const encoding = "utf8";
+
 // Asynchronous
-fs.readFile("text_file.txt", "utf8", (err, data) => {
+fs.readFile(readFilePath, encoding, (err, data) => {
   console.log("<Async>", data);
-  fs.writeFile("_text_file_copy.txt", data, () => {
+  fs.writeFile(writeFilePath, data, () => {
     console.log("<Async> Finished writing to file");
   });
 });
 
 // Synchronous
-const textFileContent = fs.readFileSync("text_file.txt", "utf8");
+const textFileContent = fs.readFileSync(readFilePath, encoding);
 console.log("<Sync>", textFileContent);
 
-fs.writeFileSync("_text_file_copy.txt", textFileContent);
+fs.writeFileSync(writeFilePath, textFileContent);
 console.log("<Sync> Finished writing to file");
