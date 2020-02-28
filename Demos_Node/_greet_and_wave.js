b7ab7ee4ea9b990c35d19c25e66d8fb89aed462e
Danilo Novakovic <dakenzi97@gmail.com>
2020-02-28 15:03:56 +0100

    Added example for multiple exports

---
+++ Demos_Node/_greet_and_wave.js
@@ -0,0 +1,9 @@
+const greet = function(name) {
+  console.log(`Hello ${name}`);
+};
+
+const wave = function(name) {
+  console.log(`*Waving towards ${name}*`);
+};
+
+module.exports = { greet, wave };
