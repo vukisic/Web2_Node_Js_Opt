0d8a49f62c7125feee6fe43763548104ee4c8d03
Danilo Novakovic <dakenzi97@gmail.com>
2020-03-01 11:07:37 +0100

    Added example for working with query

--- Demos_Node/Express_Node/app.js
+++ Demos_Node/Express_Node/app.js
@@ -6,7 +6,7 @@ app.set("view engine", "ejs"); // setting up template engine (like Razor in ASP.
 // GET requests
 
 app.get("/", (req, res) => {
-  res.send("this is a homepage"); // text as response
+  res.send(`this is a homepage. query: ${JSON.stringify(req.query)}`); // text as response
 });
 
 app.get("/contact", (req, res) => {
