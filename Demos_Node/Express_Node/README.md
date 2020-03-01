9ffe1f1c3ebdea74c399570b7d11d22cd92d51f4
Danilo Novakovic <dakenzi97@gmail.com>
2020-03-01 10:42:47 +0100

    Added simple example for Node.js using Express

---
+++ Demos_Node/Express_Node/README.md
@@ -0,0 +1,4 @@
+# Getting started
+
+1. Type `npm install` in cmd/terminal (only once)
+2. To run a file `node <filename>` (or `npm start` to run main/default example)
---
+++ Demos_Node/Express_Node/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs"); // setting up template engine (like Razor in ASP.NET)
+
+// GET requests
+
+app.get("/", (req, res) => {
+  res.send("this is a homepage"); // text as response
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/contact.html"); // serving static html
+});
+
+app.get("/student/:id", (req, res) => {
+  let data = { age: 23, name: "Djura Djuric", id: req.params.id };
+  res.render("student", { student: data }); // serving (rendering) files using view engine (default path views/<viewname>)
+});
+
+// Listen
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}...`);
+});
